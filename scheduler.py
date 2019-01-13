df344c614735c47f230169aff388afebf1a5480a
jdegene <j-f-d@gmx.de>
2019-01-13 13:56:55 +0100

    cardcow added

--- scheduler.py
+++ scheduler.py
@@ -49,6 +49,18 @@ except:
     log_str = log_str + pendulum.now().to_datetime_string() + \
                          " - get_AKV() FAILED" + "\n"
 
+# cardcow
+try:
+    num_entries_before = c.execute('SELECT Count(*) FROM CrawlImgs').fetchone()[0]
+    main_handler.get_cardcow(work_fol=work_fol, conDB=conDB, c=c)
+    num_entries_after = c.execute('SELECT Count(*) FROM CrawlImgs').fetchone()[0]
+    log_str = log_str + pendulum.now().to_datetime_string() + \
+                         " - get_cardcow() successfully run, added lines: " + \
+                         str(num_entries_after-num_entries_before)+ "\n"
+except:
+    log_str = log_str + pendulum.now().to_datetime_string() + \
+                         " - get_cardcow() FAILED" + "\n"     
+
 # delcampe
 try:
     num_entries_before = c.execute('SELECT Count(*) FROM CrawlImgs').fetchone()[0]
