1c7be6e9f75785a6742fb3d14c17f690cb3d7d53
jdegene <j-f-d@gmx.de>
2019-01-13 11:29:55 +0100

    get_postcardshopping added to scheduler

--- scheduler.py
+++ scheduler.py
@@ -109,6 +109,17 @@ except:
     log_str = log_str + pendulum.now().to_datetime_string() + \
                          " - get_oldthing() FAILED"  + "\n"
 
+# postcardshopping
+try:
+    num_entries_before = c.execute('SELECT Count(*) FROM CrawlImgs').fetchone()[0]
+    main_handler.get_postcardshopping(work_fol=work_fol, conDB=conDB, c=c)
+    num_entries_after = c.execute('SELECT Count(*) FROM CrawlImgs').fetchone()[0]
+    log_str = log_str + pendulum.now().to_datetime_string() + \
+                         " - get_postcardshopping() successfully run, added lines: " + \
+                         str(num_entries_after-num_entries_before) + "\n"
+except:
+    log_str = log_str + pendulum.now().to_datetime_string() + \
+                         " - get_postcardshopping() FAILED"  + "\n"
 
 
 # etsy
