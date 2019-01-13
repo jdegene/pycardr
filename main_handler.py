077755fa0761292fe6072c9ee8a1606e18cec171
jdegene <j-f-d@gmx.de>
2019-01-13 11:25:55 +0100

    get_postcardshopping() implemented

--- main_handler.py
+++ main_handler.py
@@ -440,11 +440,51 @@ def get_oldthing(work_fol=work_fol, conDB=conDB, c=c, searchterm_json=searchterm
 
 
 
+def get_postcardshopping(work_fol=work_fol, conDB=conDB, c=c):
+    ''' uses no search phrases, skims thtough entire collection '''
+    
+    cur_page = 0 # starting page to crawl (will instantly incremented by 1, so 0 is first page)
+    existing_share = 0
 
+    while existing_share < 0.9:
+        cur_page = cur_page + 1
+        get_page = website_handling.postcardshopping("", page=cur_page)
+        
+        if len(get_page) < 2:
+            print("Leaving page, <2 items found")
+            break
 
+        exisiting_imgs_int = 0 # how many imgs on site are already in DB
 
+        # loop over every image on loaded site
+        for image in get_page:           
+        
+            # return 0 if image id is not in database yet
+            imageID_found = image_handling.checkID(image['entry_id'], c, subsite='postcardshopping')
+            exisiting_imgs_int = exisiting_imgs_int + min(imageID_found, 1)
+            existing_share = exisiting_imgs_int / len(get_page) 
 
+            if imageID_found == 0:
+                # load image url from web to PIL
+                img = Image.open(BytesIO(requests.get(image['thumb_url']).content))
 
+                # cheack image's hashes against true images
+                lowest_dhash = image_handling.check_all_hashes(img, 
+                                 (image['thumb_url'], image['entry_url'],
+                                  'postcardshopping', "", image['entry_id']), 
+                                 work_fol,
+                                 c, 
+                                 conDB, 
+                                 threshold=13)
+                
+                # send email notification for every image dHash < 10
+                if lowest_dhash < 10:
+                    website_handling.sendMail("dHash" + str(lowest_dhash) + "found. Check out \n" + 
+                                                  image['thumb_url'] + "\n" +  image['entry_url'] + "\n" +  
+                                                  str(image['entry_id']))
+
+        print("Last page share was", existing_share)
+        
 """
 get_AK()
 get_oldthing()
@@ -453,6 +493,7 @@ get_Ebay()
 get_delcampe()
 get_hippostcard()
 get_googleimgs()
+get_postcardshopping()
 """
 
 """
