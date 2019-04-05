dc8858b25542dddc60d7846eb0746d34f846b800
jdegene <j-f-d@gmx.de>
2019-04-05 18:00:02 +0200

    fixed bad thumb_url extraction method for todocoleccion

--- website_handling.py
+++ website_handling.py
@@ -1118,8 +1118,7 @@ def todocoleccion(website='postales-de-galantes-y-mujeres', page=1):
         
         entry_url = "https://www.todocoleccion.net" + entry.find('a')['href']
         
-        thumb_url_long = entry.find('img')['data-original']
-        thumb_url = thumb_url_long[ : thumb_url_long.find('.jpg')+4] # cutoff non interesting scaling part
+        thumb_url = entry.find('a')['data-image-url']
     
         entry_id = entry_url[ entry_url.rfind('~') + 1 : ]
 
